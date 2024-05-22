4c62a17192aaf19d18fcd061d8654cfe5480d77f
4kavatarmrzk <165391460+MarzkX@users.noreply.github.com>
2024-05-22 20:44:44 +0900

    BIN

---
+++ bin/setup/setup-unix.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# SETUP FOR MAC AND LINUX SYSTEMS!!!
+#
+# REMINDER THAT YOU NEED HAXE INSTALLED PRIOR TO USING THIS
+# https://haxe.org/download/version/4.2.5/
+haxelib install lime 8.1.2
+haxelib install openfl 9.3.3
+haxelib install flixel 5.8.0
+haxelib run lime setup flixel
+haxelib run lime setup
+haxelib install flixel-tools 1.5.1
+haxelib run flixel-tools setup
+haxelib install SScript 8.1.6
+haxelib install hxCodec 2.5.1
+haxelib install tjson 1.4.0
+haxelib install yagp 1.1.4
+haxelib git systools https://github.com/waneck/systools
+haxelib git tentools https://github.com/TentaRJ/tentools
+haxelib git haxeui-core https://github.com/haxeui/haxeui-core
+haxelib git haxeui-flixel https://github.com/haxeui/haxeui-flixel
+haxelib git hxcpp-debug-server https://github.com/FunkinCrew/hxcpp-debugger
+haxelib git thx.core https://github.com/FunkinCrew/thx.core
+haxelib git thx.semver https://github.com/FunkinCrew/thx.semver
+haxelib git flixel-ui https://github.com/MarzkX/flixel-ui essence
+haxelib git flixel-addons https://github.com/MarzkX/flixel-addons essence
+haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
+haxelib remove linc_luajit
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
+haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
